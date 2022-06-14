27ca65c9f4d18a562529f28ae2ca1c97b3b83ffa
Leonel Oliveira <leoneloliveira@VOL-VHQ-ML7261.local>
2022-06-14 16:34:44 -0400

    Fixes to image result page

--- components/ImageResults.js
+++ components/ImageResults.js
@@ -8,25 +8,27 @@ export default function ImageResults({ results }) {
         {results.items?.map((result) => (
           <div key={result.link} className="mb-8">
             <div className="group">
+              
               <a href=''>
                 <img
-                  className="group-hover:shadow-xl w-full h-60 object-contain"
+                  className="group-hover:shadow-xl w-full h-60 object-contain mb-3 bg-gray-100 p-2"
                   src={result.link}
                   alt={result.title}
                 />
               </a>
+
               <a className="group-hover:underline" href=''>
-                <h2 className="truncate text-xl">{result.title}</h2>
+                <h2 className="truncate text-md">{result.title}</h2>
               </a>
               <a className="group-hover:underline" href=''>
-                <p className="text-gray-600">{result.displayLink}</p>
+                <p className="text-gray-600 text-sm">{result.displayLink}</p>
               </a>
             </div>
           </div>
         ))}
       </div>
       <div className="ml-16">
-          {/* <PaginationButtons/> */}
+        {/* <PaginationButtons/> */}
       </div>
     </div>
   );
