453725e6d06b7cd17f5775a3fb2b0cf3d0531442
Leonel Oliveira <leoneloliveira@VOL-VHQ-ML7261.local>
2022-06-28 13:55:20 -0400

    Added function to Feeling lucky btn

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,15 @@ export default function Home() {
     event.preventDefault();
     const term = searchInputRef.current.value;
     if(!term.trim()) return;
-    router.push(`/search?term=${term.trim()}`)
-
-    
+    router.push(`/search?term=${term.trim()}&searchType=`);
+  }
+  async function randomSearch(event) {
+    event.preventDefault();
+    const randomTerm = await fetch(
+      "https://random-word-api.herokuapp.com/word?number=1"
+    ).then((response) => response.json());
+    if (!randomTerm) return;
+    router.push(`/search?term=${randomTerm}&searchType=`);
   }
   
   return (
@@ -54,7 +60,7 @@ export default function Home() {
           <button className="btn" onClick={search}>
             Google Search
           </button>
-          <button className="btn">
+          <button className="btn" onClick={randomSearch}>
             I&apos;m Feeling Lucky
           </button>
         </div>
